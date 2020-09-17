959cc7bce3095558a29f406d58b5cf27605413df
ShaynePierce <shayne.pierce@gmail.com>
2020-09-17 13:58:50 -0600

    without alert

--- js/script.js
+++ js/script.js
@@ -1,3 +1,2 @@
-alert('Hello');
 document.getElementsByClassName('copyright-year')[0].innerHTML = new Date().getFullYear();
 document.getElementsByClassName('last-updated')[0].innerHTML = document.lastModified; 
