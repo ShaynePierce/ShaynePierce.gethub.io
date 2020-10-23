3c55221469a6044e62f5a8d19f2d892dd2271078
ShaynePierce <shayne.pierce@gmail.com>
2020-10-23 17:28:00 -0600

    update

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -48,10 +48,6 @@ function getFullDateProper() {
     return `${dayNameList[currentDate.getDay()]}, ${currentDate.getDate()} ${monthNameList[currentDate.getMonth()]} ${currentDate.getFullYear()}`; 
 }
 
-function getWindChill(t, s) {
-  return  Math.round(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));  
-}
-
 document.getElementsByClassName('copyright-year')[0].innerHTML = new Date().getFullYear();
 document.getElementsByClassName('current-date')[0].innerHTML = getFullDateProper();
 
--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -3,6 +3,10 @@ let wind = 5;
 document.querySelector(".wind-speed").innerHTML = wind;
 document.querySelector(".current-temp").innerHTML = temp;
 
+function getWindChill(t, s) {
+  return  Math.round(35.74 + (0.6215 * t) - (35.75 * Math.pow(s, 0.16)) + (0.4275 * t * Math.pow(s, 0.16)));  
+}
+
 if (temp < 50 && wind > 3)  {
   document.querySelector(".wind-chill").innerHTML = getWindChill(temp, wind);
 } else {
