fe26b62c6e542608228e20a18882cd44b24fd702
ShaynePierce <shayne.pierce@gmail.com>
2020-11-14 08:13:39 -0700

    lesson9

---
+++ lesson9/js/cities.js
@@ -0,0 +1,48 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+async function getJSON() {
+    try {
+        let res = await fetch(requestURL);
+        return await res.json();
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function rendercities() {
+    let citiesJSON = await getJSON();
+    let cities = citiesJSON['towns'];
+
+    let html = '';
+    html = addCity(cities, 'Preston') + addCity(cities, 'Soda Springs') + addCity(cities, 'Fish Haven');
+
+    let container = document.querySelector('.cities');
+    container.innerHTML = html;
+}
+
+function addCity(cities, cityName) {
+    let htmlSegment =  '';
+    cities.forEach(city => {
+        if (city.name == cityName) {
+            let pop = city.currentPopulation.toLocaleString(undefined);
+            let rain = Math.round(city.averageRainfall * 10) / 10;    
+
+            htmlSegment =  `
+                <section class="city">                            
+                    <picture>
+                        <img src="images/${city.photo}" alt="Photo of ${city.name}" >
+                    </picture>
+                    <div class="city-info">
+                        <h2>${city.name}</h2>
+                        <p class="motto">${city.motto}</p>
+                        <p class="year-founded">&bull; Year Founded: <span class="city-data">${city.yearFounded}<span></p>
+                        <p class="population">&bull; Population: <span class="city-data">${pop}<span></p>        
+                        <p class="annual-rain">&bull; Annual Rain Fall: <span class="city-data">${rain}"<span></p>        
+                    </div>
+                </section>`;                
+        }
+    });
+    return htmlSegment;
+}
+
+rendercities();
